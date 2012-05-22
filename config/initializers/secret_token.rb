8e9cf19f34e93e318ddd6dfd103faf2a10e61892
Jason Bynum <jason@pignite.com>
2012-05-21 22:40:05 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mixx96Deals::Application.config.secret_token = '116ea194a965d7be686d9c8509a308ac6786736790a1ceb1100e24e999f9f230eae46d18a6ae73bccd31de5768ccf3f79bdeddca4a4faccfb9e7ffbb90ae081e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mixx96Deals::Application.config.session_store :cookie_store, key: '_mixx96_deals_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mixx96Deals::Application.config.session_store :active_record_store
