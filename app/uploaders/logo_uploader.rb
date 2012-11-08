4f9f7b878d048a954d22ba861c1c01b6da3f303e
Jason Bynum <jason@pignite.com>
2012-11-08 14:33:58 -0800

    updated images

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -29,7 +29,7 @@ class LogoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-   process :resize_to_fill => [360, 268]
+   process :resize_to_fill => [360, 270]
   #
   # def scale(width, height)
   #   # do something
