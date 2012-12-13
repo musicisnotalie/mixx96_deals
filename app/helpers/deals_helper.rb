70eff1e1c9d5bf7844e4cddc46506d6a546965cc
Jason Bynum <jason@pignite.com>
2012-12-12 23:22:57 -0800

    updated savings formula and some show cleanup

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -1,7 +1,7 @@
 module DealsHelper
 
   def deal_savings(deal)
-    number_to_percentage((deal.price.to_f / deal.normal_price.to_f) * 100, precision: 0)  
+    number_to_percentage( ((deal.normal_price.to_f - deal.price.to_f) / deal.normal_price.to_f ) * 100, precision: 0)
   end
 
 end
